6340354de9ca7cf5d016b847403dc5b0f1433405
Axel Valdez <nahu.ruiz18@gmail.com>
2023-06-28 11:42:54 -0300

    commit

---
+++ src/main/java/com/platzi/gatos_app/Main.java
@@ -0,0 +1,38 @@
+
+package com.platzi.gatos_app;
+
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+import com.platzi.gatos_app.model.Cats;
+import com.platzi.gatos_app.service.CatService;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        int menuOption = -1;
+        String[] buttoms = {" 1. ver gatos", "2. ver favoritos", "3. salir"};
+
+        do{
+            String option = (String) JOptionPane.showInputDialog(null, "Gatitos java", "Menu principal", JOptionPane.INFORMATION_MESSAGE, null, buttoms,buttoms[0]);
+
+            for(int i=0;i<buttoms.length;i++){
+                if(option.equals(buttoms[i])){
+                    menuOption = i;
+                }
+            }
+
+            switch(menuOption){
+                case 0:
+                    CatService.seeRandomCats();
+                    break;
+                case 1:
+                    Cats cat = new Cats();
+                    CatService.seeFavoriteCats(cat.getApikey());
+                default:
+                    break;
+            }
+        }while(menuOption != 2);
+    }
+}
---
+++ src/main/java/com/platzi/gatos_app/model/FavoriteCat.java
@@ -0,0 +1,46 @@
+
+package com.platzi.gatos_app.model;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+public class FavoriteCat {
+	Dotenv dotenv = Dotenv.load();
+
+    String id;
+    String imageId;
+    String apikey= dotenv.get("API_KEY");
+    public ImageX image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(String imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public ImageX getImage() {
+        return image;
+    }
+
+    public void setImage(ImageX image) {
+        this.image = image;
+    }
+}
